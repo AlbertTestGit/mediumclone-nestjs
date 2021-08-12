a2ddfbee1fde8b9a301f2ee31ae89736a290b1d2
Amirzhan <albert.git1@gmail.com>
2021-08-12 17:13:44 +0500

    Guard для авторизации

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get, Post, Req, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
 import { LoginUserDto } from '@app/user/dto/loginUser.dto';
-import { ExpressRequestInterface } from '@app/types/expressRequest.interface';
 import { User } from '@app/user/decorators/user.decorator';
 import { UserEntity } from '@app/user/user.entity';
+import { AuthGuard } from '@app/user/guards/auth.guard';
 
 @Controller()
 export class UserController {
@@ -30,6 +30,7 @@ export class UserController {
   }
 
   @Get('user')
+  @UseGuards(AuthGuard)
   async currentUser(
     @User() user: UserEntity
   ): Promise<UserResponseInterface> {
